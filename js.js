0ee950e8b1387f97e545d48d6a677c4d8e90ea27
oleg-cmd <oleg.selanta@gmail.com>
2023-09-13 16:18:12 +0300

    major

--- js.js
+++ js.js
@@ -1,9 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var yButtons = document.querySelectorAll(".y-btns input[type='button']")
-    var rButtons = document.querySelectorAll(".r-btns input[type='button']")
-
-    // Загрузка ранее сохраненных данных при загрузке страницы
-    loadSavedData();
+    let yButtons = document.querySelectorAll(".y-btns input[type='button']")
+    let rButtons = document.querySelectorAll(".r-btns input[type='button']")
 
     yButtons.forEach(function (button) {
         button.addEventListener('click', function () {           
@@ -27,88 +24,69 @@ document.addEventListener('DOMContentLoaded', function () {
 })
 
 document.addEventListener('DOMContentLoaded', function () {
-    var submitButton = document.getElementById('submit-button')
-    var resultTable = document.querySelector('.result-table');
+    let submitButton = document.getElementById('submit-button')
+    let resultTable = document.querySelector('.result-table');
 
     submitButton.addEventListener('click', function () {
-        var selectedY = document.querySelector(
+        let selectedY = document.querySelector(
             '.y-btns input[type="button"].selected',
         )
-        var selectedR = document.querySelector(
+        let selectedR = document.querySelector(
             '.r-btns input[type="button"].selected',
         )
 
-        var xInput = document.getElementById('x-input')
+        let xInput = document.getElementById('x-input')
 
-        
         if (!selectedY) {
-            alert('Выберите значение Y.')
+            showCustomAlert('Выберите значение Y.')
             return // Останавливаем отправку формы
         }
 
-       
         if (!selectedR) {
-            alert('Выберите значение R.')
+            showCustomAlert('Выберите значение R.')
             return // Останавливаем отправку формы
         }
 
-        
-        var xValue = parseFloat(xInput.value)
+        let xValue = parseFloat(xInput.value)
 
         if (isNaN(xValue) || xValue >= 3 || xValue <= -5) {
-            alert('Введите корректное значение X в диапазоне от -5 до 3. (не включительно)')
+            showCustomAlert('Введите корректное значение X в диапазоне от -5 до 3. (не включительно)')
             return // Останавливаем отправку формы
         }
 
-        var formData = new FormData()
+        let formData = new FormData()
         formData.append('x', xValue)
         formData.append('y', selectedY.value)
         formData.append('r', selectedR.value)
 
         console.log(formData)
 
-        // Измеряем время начала выполнения
-        var startTime = performance.now()
-
         // Отправляем запрос на сервер
-        var xhr = new XMLHttpRequest()
+        let xhr = new XMLHttpRequest()
         xhr.open('POST', 'process.php', true)
-        
+        xhr.setRequestHeader('Content-Type', 'application/json'); // Устанавливаем заголовок Content-Type
+        xhr.setRequestHeader('Accept', 'application/json'); // Устанавливаем заголовок Accept
+
         xhr.onload = function () {
             if (xhr.status === 200) {
                 // Обрабатываем ответ от сервера
-                var response = JSON.parse(xhr.responseText)
-                
-                var endTime = performance.now()
-                var executionTime = endTime - startTime
+                let response = JSON.parse(xhr.responseText)
 
-                var newRow = document.createElement('div')
+                let newRow = document.createElement('div')
                 newRow.classList.add('row')
-                
+
                 newRow.innerHTML = `
                     <div class="x">${xValue}</div>
                     <div class="y">${selectedY.value}</div>
                     <div class="r">${selectedR.value}</div>
-                    <div class="ct">${getCurrentTime()}</div>
-                    <div class="et">${executionTime} ms</div>
+                    <div class="ct">${response.serverTime}</div>
+                    <div class="et">${response.executionTime} ms</div>
                     <div class="result">${response.result}</div>
                 `
 
-
                 resultTable.appendChild(newRow)
-               // Сохранение данных в куки
-                saveDataToCookie({
-                    x: xValue,
-                    y: selectedY.value,
-                    r: selectedR.value,
-                    currentTime: getCurrentTime(),
-                    executionTime: executionTime,
-                    result: response.result
-                });
-
-               
             } else {
-                alert('Ошибка при отправке данных на сервер.')
+                showCustomAlert('Ошибка при отправке данных на сервер.')
             }
         }
 
@@ -116,74 +94,23 @@ document.addEventListener('DOMContentLoaded', function () {
     })
 })
 
-function getCurrentTime() {
-    var now = new Date();
-    var hours = now.getHours().toString().padStart(2, '0');
-    var minutes = now.getMinutes().toString().padStart(2, '0');
-    var seconds = now.getSeconds().toString().padStart(2, '0');
-    return hours + ':' + minutes + ':' + seconds;
-}
-
-var clearButton = document.querySelector('.main-btns div div:last-child');
+let clearButton = document.querySelector('.main-btns div div:last-child');
 
 clearButton.addEventListener('click', function () {
-    var resultTable = document.querySelector('.result-table');
-    var firstRow = resultTable.querySelector('.row');
+    let resultTable = document.querySelector('.result-table');
+    let firstRow = resultTable.querySelector('.row');
     // Удаляем все строки, начиная с второй
     while (resultTable.children.length > 1) {
         resultTable.removeChild(resultTable.children[1]);
     }
-
-    // Очистка куков с именем 'savedData'
-    clearCookie('savedData');
 });
 
-// Функция для очистки куки по имени
-function clearCookie(name) {
-    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
-}
+function showCustomAlert(message) {
+    const customAlert = document.querySelector('.custom-alert');
+    customAlert.textContent = message;
+    customAlert.style.opacity = '1';
 
-function setCookie(name, value, days) {
-    const expires = new Date();
-    expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
-    document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
-}
-
-
-function getCookie(name) {
-    const cookieValue = document.cookie.match(`(^|;)\\s*${name}=([^;]+)`);
-    return cookieValue ? cookieValue.pop() : '';
-}
-
-function loadSavedData() {
-    const savedDataJSON = getCookie('savedData');
-    if (savedDataJSON) {
-        const savedDataArray = JSON.parse(savedDataJSON);
-        savedDataArray.forEach(function (savedData) {
-            var newRow = document.createElement('div')
-            newRow.classList.add('row')
-
-            newRow.innerHTML = `
-                <div class="x">${savedData.x}</div>
-                <div class="y">${savedData.y}</div>
-                <div class="r">${savedData.r}</div>
-                <div class="ct">${savedData.currentTime}</div>
-                <div class="et">${savedData.executionTime} ms</div>
-                <div class="result">${savedData.result}</div>
-            `
-            var resultTable = document.querySelector('.result-table');
-            resultTable.appendChild(newRow);
-        });
-    }
-}
-
-function saveDataToCookie(data) {
-    const savedDataJSON = getCookie('savedData');
-    let savedDataArray = [];
-    if (savedDataJSON) {
-        savedDataArray = JSON.parse(savedDataJSON);
-    }
-    savedDataArray.push(data);
-    const dataJSON = JSON.stringify(savedDataArray);
-    setCookie('savedData', dataJSON, 365);
+    setTimeout(() => {
+        customAlert.style.opacity = '0';
+    }, 10000); 
 }
--- process.php
+++ process.php
@@ -1,15 +1,21 @@
 <?php
-// Получаем данные
-$x = floatval($_POST['x']);
-$y = floatval($_POST['y']);
-$r = floatval($_POST['r']);
 
-if (   
-    ($x > -5 && $x < 3) &&
-    ($y >= -4 && $y <= 4) &&
-    ($r >= 1 && $r <= 3)
-) {
-   
+session_start();
+
+function validateData($x, $y, $r) {
+    if (
+        is_numeric($x) && is_numeric($y) && is_numeric($r) &&
+        ($x > -5 && $x < 3) &&
+        ($y >= -4 && $y <= 4) &&
+        ($r >= 1 && $r <= 3)
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkHit($x, $y, $r) {
     if (
         // квадрат
         ($x <= $r && $x >= 0 && $y >= 0 && $y <= $r) ||
@@ -18,16 +24,50 @@ if (
         // четверть сферы
         ($x >= -$r / 2 && $x <= 0 && $y >= -$r / 2 && $y <= 0 && ($x ** 2 + $y ** 2) <= ($r / 2) ** 2)
     ) {
-        $result = "Попадание";
+        return "Попадание";
     } else {
-        $result = "Промах";
+        return "Промах";
     }
-} else {
-    // Значения не соответствуют
-    http_response_code(400); // "Bad Request"
-    $result = "Неверные значения. Пожалуйста, убедитесь, что x в диапазоне [-5, 3], y в диапазоне [-4, 4] и r в диапазоне [1, 3].";
 }
 
-echo json_encode(['result' => $result]);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $requestData = json_decode(file_get_contents('php://input'), true);
 
+    $x = floatval($requestData['x']);
+    $y = floatval($requestData['y']);
+    $r = floatval($requestData['r']);
+
+    if (validateData($x, $y, $r)) {
+        $result = checkHit($x, $y, $r);
+        $serverTime = date('Y-m-d H:i:s T'); // Формат времени (год-месяц-день час:минута:секунда часовой_пояс)
+
+         // Вычисляем execution time
+        $startTimestamp = $_SESSION['startTimestamp'];
+        $endTimestamp = time();
+        $executionTime = ($endTimestamp - $startTimestamp) * 1000; // в миллисекундах
+
+        $response = [
+            'result' => $result,
+            'serverTime' => $serverTime,
+            'executionTime' => $executionTime,
+        ];
+
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    } else {
+        http_response_code(400); // "Bad Request"
+        $result = "Неверные значения. Пожалуйста, убедитесь, что x в диапазоне [-5, 3], y в диапазоне [-4, 4] и r в диапазоне [1, 3].";
+
+        $response = [
+            'result' => $result,
+            'serverTime' => null,
+            'executionTime' => null,
+        ];
+        header('Content-Type: application/json');
+        echo json_encode($response);
+    }
+} else {
+    http_response_code(405); // "Method Not Allowed"
+    echo "Метод не разрешен.";
+}
 ?>
